327b880ce478bbfe0135e80de42b8dbdb20b3a01
RapRap <jembercity.rafi0051@gmail.com>
2023-07-06 23:39:27 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,7 @@ Route::resource('employees', EmployeeController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
